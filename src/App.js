b436fd06a6e4301bdba4ffbe9f2b0e5685a2b57c
chrisisler <christopherisler1@gmail.com>
2018-07-24 23:42:19 -0400

    improved css, added Sound component

--- src/App.js
+++ src/App.js
@@ -2,30 +2,54 @@
 // Place a left-to-right scrolling sound item selector at top of view.
 // Place a play button in the middle.
 
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
+import PropTypes from 'prop-types'
 
 import './App.css'
 
-const SoundItemMock = () => (
-  <div dataSound="74" class="sound-item">
-    <kbd>sound-item-name</kbd>
-    <audio dataSound="74" src="./sounds/kick.wav" />
-  </div>
-)
+class Sound extends Component {
+  static propTypes = {
+    node: PropTypes.node,
+    name: PropTypes.string.isRequired,
+    path: PropTypes.string.isRequired
+  }
+
+  node = createRef()
+
+  clicked = event => {
+    // always play sound from beginning
+    this.node.current.currentTime = 0
+    this.node.current.play()
+  }
+
+  render() {
+    const { name, path } = this.props
+
+    return (
+      <div className="sound-item" onClick={this.clicked}>
+        <kbd>{name}</kbd>
+        <audio src={path} ref={this.node} />
+      </div>
+    )
+  }
+}
 
 export default class App extends Component {
-  playSound = event => {
-    console.log('event is:', event)
-    // let e = document.querySelector(`audio[dataSound="${event.keyCode}"]`)
+  state = {
+    soundItems: []
   }
 
   render() {
     return (
       <main>
-        <div class="sound-items-container">
-          <SoundItemMock />
-          <SoundItemMock />
+        <div className="sound-items-container">
+          <Sound name="kick" path="./sounds/kick.wav" />
+          <Sound name="snare" path="./sounds/snare.wav" />
         </div>
+
+        <section className="song-container">
+          Double click a sound from above, homie.
+        </section>
       </main>
     )
   }
